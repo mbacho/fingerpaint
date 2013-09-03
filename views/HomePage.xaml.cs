bce50c087250996148d9ad41e0e431e0d46ab574
Chomba Ng'ang'a <choeringa@gmail.com>
2013-09-04 02:55:14 +0300

    Adding settings page and menu icons Correcting palette location

--- views/HomePage.xaml.cs
+++ views/HomePage.xaml.cs
@@ -1,23 +1,112 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.Windows.Media.Imaging;
+using System.IO.IsolatedStorage;
+using Microsoft.Xna.Framework.Media;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using Microsoft.Phone.Shell;
 
 namespace FingerPaint.views
 {
     public partial class HomePage : PhoneApplicationPage
     {
+        private List<SolidColorBrush> colors;
         public HomePage()
         {
             InitializeComponent();
+
+            colors = new List<SolidColorBrush>() { 
+                new SolidColorBrush(Colors.Black), new SolidColorBrush(Colors.Blue), 
+                new SolidColorBrush(Colors.Brown), new SolidColorBrush(Colors.Cyan), 
+                new SolidColorBrush(Colors.DarkGray), new SolidColorBrush(Colors.Gray), 
+                new SolidColorBrush(Colors.Green) ,new SolidColorBrush(Colors.LightGray),
+                new SolidColorBrush(Colors.Magenta) ,new SolidColorBrush(Colors.Orange),
+                new SolidColorBrush(Colors.Purple) ,new SolidColorBrush(Colors.Red),
+                new SolidColorBrush(Colors.Yellow) 
+            };
+            lst.DataContext = colors;
+            lst.SelectionChanged += new SelectionChangedEventHandler(lst_SelectionChanged);
+        }
+
+        private void lst_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            rct.Stroke = (SolidColorBrush)((ListBox)sender).SelectedItem;
+        }
+
+        protected override void OnNavigatingFrom(System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            PhoneApplicationService.Current.State["color"] = lst.SelectedItem;
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (PhoneApplicationService.Current.State.ContainsKey("color"))
+                lst.SelectedItem = PhoneApplicationService.Current.State["color"];
+        }
+
+        private void rct_MouseMove(object sender, MouseEventArgs e)
+        {
+            foreach (StylusPoint p in e.StylusDevice.GetStylusPoints(canvas))
+            {
+                paintShape((SolidColorBrush)lst.SelectedItem, p, new Ellipse());
+            }
+        }
+
+        private void paintShape(Brush color, StylusPoint p, Shape shape)
+        {
+            shape.SetValue(Canvas.LeftProperty, p.X);
+            shape.SetValue(Canvas.TopProperty, p.Y);
+            shape.Opacity = p.PressureFactor;
+            shape.Width = 20d;
+            shape.Height = 20d;
+            shape.IsHitTestVisible = false;
+            shape.Stroke = shape.Fill = rct.Stroke;
+            canvas.Children.Add(shape);
+        }
+
+        private bool pointInRect(Point p)
+        {
+            return true;
+
+            Point origin = new Point(rct.Clip.Bounds.X, rct.Clip.Bounds.Y);
+            double height = rct.Height, width = rct.Width;
+            double h_temp = p.X - origin.X, w_temp = p.Y - origin.Y;
+            return (h_temp > 0) && (w_temp > 0) && (h_temp < height) && (w_temp < width);
+        }
+
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            WriteableBitmap img = new WriteableBitmap(canvas, null);
+            img.Invalidate();
+            string imgName = "test.jpg";
+            using (IsolatedStorageFile iso = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                IsolatedStorageFileStream isofs = iso.OpenFile(imgName, System.IO.FileMode.OpenOrCreate);
+                img.SaveJpeg(isofs, img.PixelWidth, img.PixelHeight, 0, 100);
+                isofs.Seek(0, System.IO.SeekOrigin.Begin);
+                MediaLibrary lib = new MediaLibrary();
+                Picture pic = lib.SavePicture(imgName, isofs);
+                isofs.Close();
+                iso.DeleteFile(imgName);
+            }
+        }
+
+        private void mnuClear_Click(object sender, EventArgs e)
+        {
+            canvas.Children.Clear();
+            canvas.Children.Add(rct);
+        }
+
+        private void mnuSettings_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/views/SettingsPage.xaml", UriKind.Relative));
         }
     }
 }
---
+++ views/SettingsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace FingerPaint.views
+{
+    public partial class SettingsPage : PhoneApplicationPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            PhoneApplicationService.Current.State["point"] = sld.Value;
+            PhoneApplicationService.Current.State["shape"] = 1;
+            PhoneApplicationService.Current.State["pressure"] = false;
+        }
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+    }
+
+}
